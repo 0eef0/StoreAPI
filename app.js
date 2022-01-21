0036a4be0ede4b1d506a66fdcfa198e6c0ead981
TheCapn-MEC <erolda266@west-mec.org>
2022-01-21 13:47:44 -0700

    modified port value

--- app.js
+++ app.js
@@ -18,10 +18,11 @@ app.get('/', (req, res) => {
     res.send('Shoes API');
 });
 
+const PORT = process.env.PORT || 3000;
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI);
-        app.listen(3000, console.log('Server listening on port 3000....'));
+        app.listen(PORT, console.log(`Server listening on port ${PORT}....`));
     } catch(err) {
         console.log(err);
     }
